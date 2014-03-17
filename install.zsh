2841cee2744bb451e1fa9ca5913a82b174881dea
_tiii <_@tiii.de>
2014-03-17 12:15:32 +0100

    require config file. set zsh dir

--- install.zsh
+++ install.zsh
@@ -1,9 +1,15 @@
 #!/bin/zsh
-
-if [[ ! -e ./config ]]; then
+if [[ ! -e ./install.zsh ]]; then
   echo "ERROR: Script needs to be run from inside the containing folder"
   exit 1
 fi
+
+if [[ ! -e ./config ]]; then
+  echo "ERROR: missing config file."
+  exit 1
+fi
+
+ZDOTDIR=$HOME/zsh-it
 
 echo "Loading Configuration..."
 source ./config
